f2f19662b87e25e931baa7b4abbe0abd291e07f4
Maiqui Pirolli Tomé <maiquitome@gmail.com>
2021-07-30 19:04:20 -0300

    :sparkles: add double variables

---
+++ double_variables.dart
@@ -0,0 +1,12 @@
+void main() {
+  // double usa 2x mais memória do que int
+  double salary = 1250.50 * 2;
+  print("My salary is $salary");
+
+  double division = 5 / 2;
+  print(division);
+
+  // int naoVaiDarCertoComInt = division * 4;
+  double comDoubleFunciona = division * 4;
+  print(comDoubleFunciona);
+}
